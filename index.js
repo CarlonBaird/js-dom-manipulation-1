402668bf290f5240eb65988afb61f4d3f6d01327
CarlonBaird <carlyestrella@live.com>
2023-03-27 13:45:40 -0400

    1 file modified

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-let k = document.querySelctor('h1');
+let k = document.querySelector('h1');
+k.innerHTML = 'Hey, bro!';
 console.log(k);
