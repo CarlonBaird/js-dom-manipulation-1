1c9dc913f2942d6adf92898e89800a4690fcf646
CarlonBaird <carlyestrella@live.com>
2023-03-27 14:36:23 -0400

    1 file modified

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ import './style.css';
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 let k = document.querySelector('h1');
+const l = document.createElement('h1');
+l['innerHTML'] = 'Hello there';
+l['style']['color'] = 'green';
+
 k.innerHTML = 'Hey, bro!';
+document.body.append(l);
 console.log(k);
